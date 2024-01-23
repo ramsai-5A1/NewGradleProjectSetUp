6e95669d2410b67f2be1c897f4b49cd50fca0e18
Ramsai <ramsaicrony@gmail.com>
2024-01-23 16:23:05 +0530

    Factory Pattern

---
+++ DesignPatterns/FactoryPattern/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory obj = new ShapeFactory();
+        Shape shapeObj = obj.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+}
