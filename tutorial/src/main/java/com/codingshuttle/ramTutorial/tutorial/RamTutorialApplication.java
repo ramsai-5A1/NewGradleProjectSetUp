76936210be6da2967512403f1f198451af79d8a1
Ramsai <ramsaicrony@gmail.com>
2024-10-27 12:45:35 +0530

    Added GameEngine Project

---
+++ tutorial/src/main/java/com/codingshuttle/ramTutorial/tutorial/RamTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.ramTutorial.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamTutorialApplication.class, args);
+	}
+
+}
---
+++ tutorial/src/test/java/com/codingshuttle/ramTutorial/tutorial/RamTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.ramTutorial.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
